42f61a3fc9c3f85057a2ae72290d11e274e2f3e3
Miguel Rosa <nmlbstudio@gmail.com>
2021-08-10 01:59:26 +0200

    [MAIN] [v.1.0.1] Fixing a bug where no static files can't be deployed in Heroku. Info has been sent to a java file. Gitignore update to not show sensitive info

--- src/main/java/com/thepinkdev/navaidjagf/utils/UrlUtils.java
+++ src/main/java/com/thepinkdev/navaidjagf/utils/UrlUtils.java
@@ -2,18 +2,15 @@ package com.thepinkdev.navaidjagf.utils;
 
 import com.thepinkdev.navaidjagf.entities.Coord;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UrlUtils {
 
-    @Autowired FileAccessUtils fileAccessUtils;
-
     public String contructURLForApiCallFromCoord(Coord coord) {
         
         StringBuilder sb = new StringBuilder();
-        String apiKey = fileAccessUtils.readFile("./sensitive/.apikey");
+        String apiKey = Sensitive.apikey;
 
         sb.append("http://api.openweathermap.org/data/2.5/weather?lat=");
         sb.append(coord.getLatitude());
